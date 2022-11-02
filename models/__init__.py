6e965ddedf911df235daaadbb1ba18c818110ff0
Darmimese <darmimesseleseyoum69@gmail.com>
2022-11-01 18:19:52 -0700

    queen

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 31 13:56:43 2022
-@authors: Dawit Getaneh
-          Darmi Seyoum
-"""
-from models.engine.file_storage import FileStorage
+"""Initialization for Models"""
 
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
