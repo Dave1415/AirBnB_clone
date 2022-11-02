6c6d0ddb0e1246c5942f7ee6aa2a849026b3ebec
Darmimese <darmimesseleseyoum69@gmail.com>
2022-11-01 17:04:05 -0700

    queen

---
+++ models/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 31 13:56:43 2022
+@authors: Dawit Getaneh
+          Darmi Seyoum
+"""
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
